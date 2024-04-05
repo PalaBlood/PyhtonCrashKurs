eb8fd769d2e84096d8f4986256c31b3d99485021
PalaBlood <mischelmantalia3@gmail.com>
2024-04-05 19:22:54 +0200

    Paar Sachen zu zahlen

---
+++ zahlen.py
@@ -0,0 +1,12 @@
+import this
+
+long_integer = 1_000_000 #=> 1000000
+#print(long_integer)
+
+
+x,y,z = 12, 14, "String"
+#print(x,y,z)
+
+KONSTANTE = 3,1415 #Typische Pyhton konvention für Konstanten
+
+print(this)
